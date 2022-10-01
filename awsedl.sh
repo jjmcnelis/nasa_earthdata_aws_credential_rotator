9b895403254bd388faa607683cb47824167fb3c7
Jack McNelis <jmcnelis@jpl.nasa.gov>
2022-10-01 03:05:48 -0400

    update script seds

--- awsedl.sh
+++ awsedl.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-
 profile=${2:-edl}
-
 output=${3:-json}
-
 if grep -q 'urs.earthdata.nasa.gov' ~/.netrc 
 then
-    printf "$(sed -e "/\[${profile}\]/,+6d" "${HOME}/.aws/credentials")"
-
+    touch ~/.aws/credentials && \
+        printf "$(sed -e "/\[${profile}\]/,+6d" "${HOME}/.aws/credentials")"
     printf "\n\n[${profile}]\noutput = ${output}\nregion = us-west-2\n"
-
     wget --auth-no-challenge -q -O - ${1:-https://archive.podaac.earthdata.nasa.gov/s3credentials} | \
-    sed 's/{//g;s/}//g;s/: / = /g;s/"//g;s/, /\n/g;s/expiration/#expiration/g' && printf "\n\n"
+        sed 's/{//g;s/}//g;s/: / = /g;s/"//g;s/, /\n/g;s/expiration/#expiration/g;s/accessKeyId/aws_access_key_id/g;s/secretAccessKey/aws_secret_access_key/g;s/sessionToken/aws_session_token/g' && \
+        printf "\n\n"
 fi
-
