34607b3a26ebe7c31f5963efdc49f0a8681251e4
Jack McNelis <jmcnelis@jpl.nasa.gov>
2022-10-04 21:36:03 -0400

    convenience updates

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # nasa_earthdata_aws_credential_rotator
 Retrieve temporary credentials provisioned by NASA Earthdata for s3 bucket access and store them in your local credentials file.
 
-An alias like this one can simplify the usage of [`awsedl.sh`](awsedl.sh).
+An alias like this one can simplify the usage of [`awsedl.sh`](awsedl.sh). (This requires that you put the script in ~/.local/bin directory and chmod it, if necessary.)
 
 ```bash
-alias awsedl='sh awsedl.sh > ./creds && mv ./creds ~/.aws/credentials'
+alias awsedl='touch ~/.aws/credentials && mv ~/.aws/credentials ~/.aws/credentials.bak && sh ~/.local/bin/awsedl.sh ~/.aws/credentials.bak > ~/.aws/credentials && echo "# New AWS tokens $(grep edl ~/.aws/credentials -A 6 | grep expiration | sed 's/#//g')"'
 ```
 
-(Warning: this recklessly overwrites your existing credentials file with outputs from `awsedl.sh`.)
+Warning: this recklessly overwrites your existing credentials file with outputs from `awsedl.sh`! (But it stores a backup in ~/.aws/credentials.bak.)
--- awsedl.sh
+++ awsedl.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-profile=${2:-edl}
-if grep -q 'urs.earthdata.nasa.gov' ~/.netrc 
+if grep -q "urs.earthdata.nasa.gov" "${HOME}/.netrc"
 then
-    mkdir -p ~/.aws && touch ~/.aws/credentials && \
-        printf "$(sed -e "/\[${profile}\]/,+6d" "${HOME}/.aws/credentials")"
-    printf "\n\n[${profile}]\noutput = json\nregion = us-west-2\n"
-    wget --auth-no-challenge -q -O - ${1:-https://archive.podaac.earthdata.nasa.gov/s3credentials} | \
+    mkdir -p ~/.aws && touch "${1:-"${HOME}/.aws/credentials"}" && \
+        echo "$(sed -e "/\[${3:-"edl"}\]/,+6d" "${1:-"${HOME}/.aws/credentials"}")" | awk 'NF {p=1} p'
+    printf "\n[${3:-"edl"}]\n" && echo "output = json" && echo "region = us-west-2"
+    wget --auth-no-challenge -q -O - "${2:-"https://archive.podaac.earthdata.nasa.gov/s3credentials"}" | \
         sed 's/{//g;s/}//g;s/: / = /g;s/"//g;s/, /\n/g;s/expiration/#expiration/g;s/accessKeyId/aws_access_key_id/g;s/secretAccessKey/aws_secret_access_key/g;s/sessionToken/aws_session_token/g' && \
         printf "\n\n"
 fi
