b54cf546ec5fe70a38a382de514921324255fff9
Jack McNelis <jjmcnelis@outlook.com>
2022-10-13 01:20:33 -0400

    Update awsedl.sh

--- awsedl.sh
+++ awsedl.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if grep -q "urs.earthdata.nasa.gov" "${HOME}/.netrc"
 then
-    [[ -f $1 ]] && printf "$(sed -e "/\[${3:-"edl"}\]/,+6d" "$1")\n\n"
+    if [[ -f $1 ]];
+        then printf "$(sed -e "/\[${3:-"edl"}\]/,+6d" "$1")\n\n";
+    fi
     printf "[${3:-"edl"}]\n" && echo "output = json" && echo "region = us-west-2"
     wget --auth-no-challenge -q -O - "${2:-"https://archive.podaac.earthdata.nasa.gov/s3credentials"}" | \
         sed 's/{//g;s/}//g;s/: / = /g;s/"//g;s/, /\n/g;s/expiration/#expiration/g;s/accessKeyId/aws_access_key_id/g;s/secretAccessKey/aws_secret_access_key/g;s/sessionToken/aws_session_token/g'
